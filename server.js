03565d88682771c48376409ff1ba0f530deb510b
Nick Shapoff <nicholasshapoff@gmail.com>
2022-10-09 12:07:26 -0700

    buttons&new

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
     res.send('Wecome to an Awesome App about Breads!')
