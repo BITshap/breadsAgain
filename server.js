bb56e17c7474d6b5da749bc0387fcdc433541b32
Nick Shapoff <nicholasshapoff@gmail.com>
2022-10-09 07:47:43 -0700

    index/show data

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express()
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.send('Wecome to an Awesome App about Breads!')
@@ -18,4 +19,8 @@ const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
 app.listen(PORT, () => {
     console.log('listening on port', PORT);
+})
+
+app.get('*', (req,res) => {
+    res.send('404')
 })
