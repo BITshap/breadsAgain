fcb63a696cf8dca9bfb56491c7a2b8819c8cbdf2
Nick Shapoff <nicholasshapoff@gmail.com>
2022-10-09 07:55:55 -0700

    new 404 page

---
+++ views/404.jsx
@@ -0,0 +1,10 @@
+const React = require('react')
+const Default = require('./layouts/default')
+
+function Error(){
+    return(
+        <li><a href="/breads">More Bread</a></li>
+    )
+}
+
+module.exports = Error
