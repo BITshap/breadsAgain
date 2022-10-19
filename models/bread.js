8ca3bb7d64ab9d7e0581e3e75d070abd76c586e4
Nick Shapoff <nicholasshapoff@gmail.com>
2022-10-19 14:59:46 -0700

    bakers

--- models/bread.js
+++ models/bread.js
@@ -4,7 +4,12 @@ const { Schema } = mongoose
 const breadSchema = new Schema ({
   name: {type: String, required: true},
   hasGluten: Boolean,
-  image: {type: String, default: 'https://static01.nyt.com/images/2013/03/03/magazine/03wmt1/03wmt1-articleLarge-v2.jpg?quality=75&auto=webp&disable=upscale'}
+  image: {type: String, default: 'https://static01.nyt.com/images/2013/03/03/magazine/03wmt1/03wmt1-articleLarge-v2.jpg?quality=75&auto=webp&disable=upscale'},
+  ingredients: ['flour ', 'water ', 'yeast.'],
+  baker: {
+    type: String,
+    enum: ['Rachel', 'Monica', 'Joey', 'Chandler', 'Ross', 'Phoebe']
+  }
 })
 
 const Bread = mongoose.model('Bread', breadSchema)
--- views/show.jsx
+++ views/show.jsx
@@ -1,7 +1,7 @@
 const React = require('react')
 const Default = require ('./layouts/default')
 
-function Show ({bread, index}) {
+function Show ({bread}) {
     console.log(bread.name)
     return (
         <Default>
@@ -17,11 +17,13 @@ function Show ({bread, index}) {
                 have gluten.
             </p>
             <img src={bread.image} alt={bread.name}/>
+            <p>Baked by {bread.baker}</p>
             <p>Ingredients: {bread.ingredients}</p>
-            <a href={`/breads/${index}/edit`}><button>Edit</button></a>
-            <form action={`/breads/${index}?_method=DELETE`} method="POST">
-                <input type='submit' value="DELETE"/>
-            </form>
+            <a href={`/breads/${bread.id}/edit`}><button>Edit</button></a>
+            <form action={`/breads/${bread.id}?_method=DELETE`} method="POST">
+  <input type='submit' value="DELETE"/>
+</form>
+
             <li><a href="/breads">Go Home</a></li>
         </Default>
     )
